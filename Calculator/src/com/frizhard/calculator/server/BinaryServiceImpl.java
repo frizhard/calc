c5df3605bf22c6d3787b31a7ef69ae11f47f57b2
Jose Servet <frizhard@gmail.com>
2015-08-06 20:07:22 +0200

    add persistence through JDO

--- Calculator/src/com/frizhard/calculator/server/BinaryServiceImpl.java
+++ Calculator/src/com/frizhard/calculator/server/BinaryServiceImpl.java
@@ -1,17 +1,36 @@
 package com.frizhard.calculator.server;
 
-import com.frizhard.calculator.client.BinaryService;
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManager;
+import javax.jdo.PersistenceManagerFactory;
 
+import com.frizhard.calculator.client.BinaryService;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
+
 @SuppressWarnings("serial")
 public class BinaryServiceImpl extends RemoteServiceServlet implements
 		BinaryService {
 
+	private String binarizeNumber(String number) {
+		return "01110011";
+	}
+	
 	@Override
 	public String convertToBinary(String number)
 			throws IllegalArgumentException {
-		// TODO Auto-generated method stub
-		return "01110011";
+		String binaryNumber = binarizeNumber(number);
+		
+		PersistenceManagerFactory pmf = JDOHelper.getPersistenceManagerFactory("transactions-optional");
+		PersistenceManager pm = pmf.getPersistenceManager();
+		
+		BinaryRequest immutableRequest = new BinaryRequest(number, binaryNumber);
+		try {
+			pm.makePersistent(immutableRequest);
+		} finally {
+			pm.close();
+		}
+		
+		return binaryNumber;
 	}
 
 }
